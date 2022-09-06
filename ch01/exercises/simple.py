ca7edf7457044f3fa53c7f64d5ccaa7f66ef5c69
Roisin Smith <112585302+rsmith0323@users.noreply.github.com>
2022-09-06 23:05:57 +0000

    working with data exercise

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,13 @@
+print(10 * 5)
+print(10 ** 2)
+print(15 / 10)
+print(15 // 10)
+print(-15 // 10)
+print(15 % 10)
+print(10 % 15)
+print(10 % 10)
+print(0 % 10)
+print(10 / 15)
+# 10/15 is a infinite decimal but when it is printed by the program it ends
+
+input("What is the current exchage rate for the Euro to Dollar?")
