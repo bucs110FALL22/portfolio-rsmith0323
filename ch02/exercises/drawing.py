f21d450307fd8cfd00abbf34330ab96937f884d6
Roisin Smith <112585302+rsmith0323@users.noreply.github.com>
2022-09-12 22:01:54 +0000

    exercise drawing with turtle

---
+++ ch02/exercises/drawing.py
@@ -0,0 +1,20 @@
+import turtle
+
+number_sides = input("Enter the number of sides: ")
+length_sides = input("Enter the length of each side: ")
+
+window = turtle.Screen()
+my_turtle = turtle.Turtle() 
+my_turtle.shape("turtle")
+my_turtle.color("blue")
+
+
+
+
+
+
+
+
+
+
+window.exitonclick()
