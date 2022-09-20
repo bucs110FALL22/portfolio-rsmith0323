901d7ee9616a00e1459c83fc4f00eca475b55a5e
Roisin Smith <112585302+rsmith0323@users.noreply.github.com>
2022-09-20 03:54:52 +0000

    guess the number exercise

--- ch02/exercises/myturtle.py
+++ ch02/exercises/myturtle.py
@@ -5,6 +5,17 @@ my_turtle = turtle.Turtle()
 my_turtle.shape("turtle")
 my_turtle.color("purple")
 
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(50)
+
+my_turtle.color("red")
+my_turtle.penup()
+my_turtle.forward(80)
 
 
 
