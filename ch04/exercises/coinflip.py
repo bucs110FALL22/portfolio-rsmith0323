0f5be709251e0b8046d0a4be23b04f9332f3ebc5
Roisin Smith <112585302+rsmith0323@users.noreply.github.com>
2022-10-04 03:40:48 +0000

    coin flip exercise

---
+++ ch04/exercises/coinflip.py
@@ -0,0 +1,17 @@
+import turtle
+
+window = turtle.Screen() # 2.  Create a screen
+window.bgcolor('lightblue')
+
+turtle = turtle.Turtle()
+turtle.color('orange')
+turtle.shape('turtle')
+
+coinflip = 
+
+
+
+
+
+
+window.exitonclick()
