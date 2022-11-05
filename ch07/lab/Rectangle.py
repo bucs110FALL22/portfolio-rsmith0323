8298013fd3b2ace209b7640b03d95f7768f12ff9
Roisin Smith <112585302+rsmith0323@users.noreply.github.com>
2022-11-05 00:13:35 +0000

    11/4 ch07 lab

---
+++ ch07/lab/Rectangle.py
@@ -0,0 +1,15 @@
+class Rectangle:
+  def __init__(self, x, y, height, width):
+    self.x = x
+    self.y = y
+    self.height = height
+    self.width = width
+    
+def __str__(self):
+  return F"x = {self.x} y = {self.y} height = {self.height} width = {self.width}"
+
+  
+	
+
+
+  
---
+++ ch07/lab/Surface.py
@@ -0,0 +1,13 @@
+from Rectangle import Rectangle
+
+
+class Surface:
+    def __init__(self, filename, x, y, height, width):
+      self.rect = Rectangle(x,y,height,width)
+      self.image = filename
+  
+    def getRect(self):
+      return(self.rect)
+    
+    
+    
